ee126b28d814286fbdd4cd28ae7e054dff4fdad7
unknown <mycallcampbell@gmail.com>
2023-03-21 19:18:58 +0000

    added blog section

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import heroImage from "../public/rolexHero.jpg";
 export default function Home() {
   return (
     <div className={styles.container}>
+      {/*  META DATA */}
       <Head>
         <title>ROB DIAMOND</title>
         <meta name="description" content="Rolex Specialists" />
@@ -20,7 +21,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        {/* HERO for desktop and ipad*/}
+        {/* HERO SECTION*/}
         <div className={styles.heroImage}>
           <Image src={"/hero_blingToBullion.jpeg"} width={1024} height={683} />
         </div>
@@ -30,74 +31,86 @@ export default function Home() {
           <h2>ROLEX</h2>
         </div>
 
-        <div className={styles.grid}>
-          <div className={styles.heroBtn}>
-            <Link
-              href={"https://wa.me/447976753254?text=Welcome%20to%20RobDiamond"}
-            >
-              WHATSAPP ROB DIRECT
-            </Link>
-          </div>
+        <div className={styles.heroBtn}>
+          <Link
+            href={"https://wa.me/447976753254?text=Welcome%20to%20RobDiamond"}
+          >
+            WHATSAPP ROB DIRECT
+          </Link>
+        </div>
 
-          <p className={styles.collectionTitle}>COLLECTION</p>
+        {/* COLLECTIONS SECTION */}
+        <p className={styles.collectionTitle}>COLLECTION</p>
 
-          <div className={styles.collectionGrid}>
-            <Link href={"/#"}>
-              <div className={styles.grid}>
-                <Image
-                  src={"/126231wimbledon/126231wimbledon1.JPG"}
-                  width={840}
-                  height={560}
-                />
-                <div className={styles.gridContent}>
-                  <p className={styles.gridTitleRolex}>ROLEX</p>
-                  <p>DATEJUST</p>
-                </div>
+        <div className={styles.collectionGrid}>
+          <Link href={"/#"}>
+            <div className={styles.grid}>
+              <Image
+                src={"/126231wimbledon/126231wimbledon1.JPG"}
+                width={840}
+                height={560}
+              />
+              <div className={styles.gridContent}>
+                <p className={styles.gridTitleRolex}>ROLEX</p>
+                <p>DATEJUST</p>
               </div>
-            </Link>
+            </div>
+          </Link>
 
-            <Link href={"/#"}>
-              <div className={styles.grid}>
-                <Image
-                  src={"/126711CHNRrootbeer/126711CHNRrootbeer1.JPG"}
-                  width={840}
-                  height={560}
-                />
-                <div className={styles.gridContent}>
-                  <p className={styles.gridTitleRolex}>ROLEX</p>
-                  <p>GMT MASTER II</p>
-                </div>
+          <Link href={"/#"}>
+            <div className={styles.grid}>
+              <Image
+                src={"/126711CHNRrootbeer/126711CHNRrootbeer1.JPG"}
+                width={840}
+                height={560}
+              />
+              <div className={styles.gridContent}>
+                <p className={styles.gridTitleRolex}>ROLEX</p>
+                <p>GMT MASTER II</p>
               </div>
-            </Link>
+            </div>
+          </Link>
 
-            <Link href={"/#"}>
-              <div className={styles.grid}>
-                <Image
-                  src={"/116506platona/116506platona1.JPG"}
-                  width={840}
-                  height={560}
-                />
-                <div className={styles.gridContent}>
-                  <p className={styles.gridTitleRolex}>ROLEX</p>
-                  <p>DAYTONA</p>
-                </div>
+          <Link href={"/#"}>
+            <div className={styles.grid}>
+              <Image
+                src={"/116506platona/116506platona1.JPG"}
+                width={840}
+                height={560}
+              />
+              <div className={styles.gridContent}>
+                <p className={styles.gridTitleRolex}>ROLEX</p>
+                <p>DAYTONA</p>
               </div>
-            </Link>
+            </div>
+          </Link>
 
-            <Link href={"/#"}>
-              <div className={styles.grid}>
-                <Image
-                  src={"/116611LVhulk/116611LVhulk1.JPG"}
-                  width={840}
-                  height={560}
-                />
-                <div className={styles.gridContent}>
-                  <p className={styles.gridTitleRolex}>ROLEX</p>
-                  <p>SUBMARINER</p>
-                </div>
+          <Link href={"/#"}>
+            <div className={styles.grid}>
+              <Image
+                src={"/116611LVhulk/116611LVhulk1.JPG"}
+                width={840}
+                height={560}
+              />
+              <div className={styles.gridContent}>
+                <p className={styles.gridTitleRolex}>ROLEX</p>
+                <p>SUBMARINER</p>
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
+        </div>
+
+        <div className={styles.space}></div>
+
+        {/* BLOG SECTION */}
+        <div className={styles.blog}>
+          <p>THE LATEST BLOGS</p>
+          <Image
+            className={styles.blogImage}
+            src={"/blogImage.JPG"}
+            width={3360}
+            height={2240}
+          />
         </div>
       </main>
 
