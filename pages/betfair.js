10f8aeee3d22e6b020f8652a543b0762efcc4e20
unknown <mycallcampbell@gmail.com>
2023-03-31 15:55:52 +0100

    test

--- pages/betfair.js
+++ pages/betfair.js
@@ -1,33 +1,51 @@
 import React from "react";
-// import useState from "react";
+import { useState } from "react";
 
 export default function betfair() {
-  //   const [formData, setFormData] = useState({});
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("", {
+    const res = await fetch("https://identitysso.betfair.com/api/login", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-Application": "oTY1y8QkfuVZeGY4",
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: JSON.stringify(formData),
+      body: JSON.stringify(`username=${username}&password=${password}`),
     });
     const data = await res.json();
+    console.log(data);
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: [e.target.valu] });
+  const handleChangeName = (e) => {
+    setFormData({ ...formData, [e.target.username]: [e.target.value] });
+  };
+
+  const handleChangePwd = (e) => {
+    setFormData({ ...formData, [e.target.password]: [e.target.value] });
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={handleChange} />
+        <input
+          type="text"
+          id="name"
+          username="username"
+          onChange={handleChangeName}
+        />
 
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" onChange={handleChange} />
+        <label htmlFor="email">password</label>
+        <input
+          type="email"
+          id="email"
+          password="password"
+          onChange={handleChangePwd}
+        />
 
         <button type="submit">Submit</button>
       </form>
