9435c0d6eb65f49d7909f2e00ba265777839ba7e
unknown <mycallcampbell@gmail.com>
2023-04-04 08:50:59 +0100

    changes to css for desktop

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,9 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src={"/hero_daytona.JPG"} width={3360} height={2240} />
         </div>
+        <div className={styles.heroImageDesktop}>
+          <Image src={"/hero_daytona_desktop.PNG"} width={3354} height={1479} />
+        </div>
 
         <div className={styles.caption}>
           <p className={styles.readyFor}>Ready for Your Perfect</p>
@@ -145,25 +148,39 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className={styles.space}></div>
+        {/* <div className={styles.space}></div> */}
 
         {/* BLOG SECTION */}
-        <div className={styles.blog}>
-          <p>THE LATEST BLOGS</p>
-          <Image
-            className={styles.blogImage}
-            src={"/blogImage.JPG"}
-            width={3360}
-            height={2240}
-          />
-        </div>
-        <p className={styles.blogCaption}>
-          Should You Invest In a Rolex Watch or Buy Gold
-        </p>
+        <div className={styles.blogContainer}>
+          <div className={styles.blog}>
+            <p className={styles.theLatest}>THE LATEST BLOGS</p>
+            <Image
+              className={styles.blogImage}
+              src={"/blogImage_desktop.JPG"}
+              width={3357}
+              height={1473}
+            />
+            <div className={styles.blogCaption}>
+              <p>Should You Invest In a Rolex Watch or Buy Gold</p>
+            </div>
+            <div className={styles.readMoreBTN}>
+              <Link href={"/blogs"}>READ MORE</Link>
+            </div>
+          </div>
 
-        {/* CTA  */}
-        <div className={styles.heroBtn}>
-          <Link href={"/blogs"}>READ MORE</Link>
+          <div className={styles.blogContent}>
+            <p className={styles.theLatestDesktop}>THE LATEST BLOGS</p>
+
+            <p>
+              Looking for the latest and most informative blogs on Rolex? Look
+              no further! We are thrilled to announce the release of our newest
+              blogs, packed with fresh and exciting content about the
+              world-renowned luxury watch brand. Our team of expert writers have
+              worked tirelessly to deliver the latest news, trends, and insights
+              on all things Rolex. Whether you're a seasoned collector or a
+              casual admirer, our newest blogs are sure to captivate and engage.
+            </p>
+          </div>
         </div>
       </main>
 
