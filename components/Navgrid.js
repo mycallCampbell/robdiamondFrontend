c9af5db8af9920d77673914c0ac7665bc74de225
unknown <mycallcampbell@gmail.com>
2023-06-23 08:20:20 +0100

    added categroy nav

--- components/Navgrid.js
+++ components/Navgrid.js
@@ -1,17 +1,50 @@
-import React from "react";
+import { useState } from "react";
 import styles from "./Navgrid.module.css";
 import Link from "next/link";
 
 function Navgrid() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMouseOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseClosed = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className={styles.navbar}>
       <ul>
         <li>
           <Link href={"/watches"}>
-            <div className={styles.menuItem}>
-              <div>
-                <h5>Categories</h5>
-              </div>
+            <div>
+              {isOpen ? (
+                <div
+                  onMouseEnter={handleMouseClosed}
+                  className={styles.categoryClosed}
+                >
+                  <div>
+                    <h5>Categories</h5>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  onMouseLeave={handleMouseOpen}
+                  className={styles.categoryOpen}
+                >
+                  <div>
+                    <h5>Categories</h5>
+                  </div>
+                  <ul className={styles.listContainer}>
+                    <div className={styles.categoryList}>DateJust</div>
+                    <div className={styles.categoryList}>Submariner</div>
+                    <div className={styles.categoryList}>GMT MAster II</div>
+                    <div className={styles.categoryList}>Sky Dweller</div>
+                    <div className={styles.categoryList}>Daytona</div>
+                  </ul>
+                </div>
+              )}
             </div>
           </Link>
         </li>
@@ -37,7 +70,7 @@ function Navgrid() {
         </li>
 
         <li>
-          <Link href={"/blogs"}>
+          <Link href={"/guides"}>
             <div className={styles.menuItem}>
               <div>
                 <h5>Guides</h5>
