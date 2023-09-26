e6d6577c628e26f212daf805f9c708092d182626
micealcalumgannon <nitroexfusion@gmail.com>
2023-09-26 12:23:45 +0100

    added image secure url

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           <title>ROB DIAMOND</title>
           <meta name="description" content="Rolex Home Page" />
           <meta property="og:url" content="https://www.robdiamond.co.uk" />
-          <meta property="og:image:" content="https://www.robdiamond.co.uk/robdiamond_logo300.png" />
+          <meta property="og:image:secure_url" content="https://www.robdiamond.co.uk/robdiamond_logo300.png" />
           <meta property="og:type" content="website" />
           {/* <meta property="og:image:width" content="250" />
           <meta property="og:image:height" content="250" /> */}
